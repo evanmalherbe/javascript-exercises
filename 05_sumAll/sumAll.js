68408fa1431086c260a05821f1d5d18ed24e40b5
Evan Malherbe <evanmalherbe@gmail.com>
2021-10-04 15:32:08 +0200

    Passed first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let difference = num2 - num1;
+    let result = 0;
+    let i; 
+    const array = [];
+
+    for (i = num1; i <= num2; i++) {
+        array.push(i);
+    }
+
+    for (i = 0; i <= array.length -1; i++) {
+        result = result + array[i];
+    }
+
+    return result;
 
 };
 
