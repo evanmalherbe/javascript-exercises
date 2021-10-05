7debfcb37c33dec20b8326219222a137f70de030
Evan Malherbe <evanmalherbe@gmail.com>
2021-10-05 10:01:32 +0200

    Passed all tests for exercise 06_leapYears.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -23,12 +23,10 @@ const sumAll = function(num1,num2) {
     } else {
         if (largest == num1) {
             for (i = num2; i <= num1; i++) {
-                console.log(i);
                 array.push(i);
             }
         } else { 
             for (i = num1; i <= num2; i++) {
-                console.log(i);
                 array.push(i);
             } 
         }
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                console.log("Leap year!")
+                return true; 
+            } else {
+                console.log("Not a leap year.")
+                return false;  
+            }
+        } else {
+            console.log("Leap year!")
+            return true;
+        }
+    } else {
+        console.log("Not a leap year.")
+        return false;
+    }
 };
 
 // Do not edit below this line
