2f5a293638b0cb3eae2951ec473b6083af8fbe14
Evan Malherbe <evanmalherbe@gmail.com>
2021-10-05 16:42:44 +0200

    Finished exercise 7 - tempConversion and passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(faran) {
+  let ansInCels = (faran - 32) * (5 / 9);
+  let result = Math.round(ansInCels * 10) /10  ;
+  console.log(result + " Celsius");
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let ansInFaran = cels * 1.8 + 32
+  result = Math.round(ansInFaran * 10) /10;
+  
+  console.log(result + " Fahrenheit");
+  return result;
 };
 
 // Do not edit below this line
